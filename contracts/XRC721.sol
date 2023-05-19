426a03b22ccfbadd8f14eb4b66101bd7e330411a
piyushjha0409 <peeyush0409@gmail.com>
2023-05-19 23:17:42 +0530

    changes applied from pc

---
+++ contracts/XRC721.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract XRC721 is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    }
+}
+interface XRC721 {
+    /**
+     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     */
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     */
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const MAINNET_URL = process.env.XINFIN_NETWORK_URL
+const TESTNET_URL = process.env.APOTHEM_NETWORK_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
   solidity: "0.8.18",
+  networks:{
+    xinfin:{
+     url: MAINNET_URL,
+    accounts: [PRIVATE_KEY]
+    },
+    apothem:{
+      url: TESTNET_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
